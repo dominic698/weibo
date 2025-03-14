1c2930c5fe02e560c957c20be13f413b06587441
dominic <zzp208@gmail.com>
2025-03-14 18:32:25 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('/signup',[UsersController::class, 'create'])->name('signup');
