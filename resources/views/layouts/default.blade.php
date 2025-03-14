8e3ce8827815699acb67ec42922f7f011e564ede
dominic <zzp208@gmail.com>
2025-03-14 17:56:17 +0800

    模板切分

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
