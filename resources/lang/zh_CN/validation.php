21b1578667b8a9222195c7588256f3424548c75a
dominic <zzp208@gmail.com>
2025-03-17 12:30:58 +0800

    中文错误信息

---
+++ resources/lang/zh_CN/validation.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'attributes' => [
+        'name' => '名称',
+        'email' => '邮箱',
+        'password' => '密码',
+    ],
+    'custom' => [
+        'email' => [
+            'required' => '邮箱地址不能为空！',
+        ],
+    ],
+];
