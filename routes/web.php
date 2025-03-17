cba7057ed51a48c1d722bdb5f8a1a1a294f6ec7a
dominic <zzp208@gmail.com>
2025-03-17 15:02:39 +0800

    创建回话

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,6 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('/signup',[UsersController::class, 'create'])->name('signup');
 Route::resource('/users', 'UsersController');
+Route::get('login',[SessionsController::class, 'create'])->name('login');
+Route::post('login',[SessionsController::class, 'store'])->name('login');
+Route::get('logout',[SessionsController::class, 'destroy'])->name('logout');
