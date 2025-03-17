5c1e959ebf73431ab849393d6c28cb50adfc382e
dominic <zzp208@gmail.com>
2025-03-17 17:39:22 +0800

    更新用户资料

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,12 @@ use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-Route::get('/signup',[UsersController::class, 'create'])->name('signup');
+Route::get('/signup', [UsersController::class, 'create'])->name('signup');
 Route::resource('/users', 'UsersController');
-Route::get('login',[SessionsController::class, 'create'])->name('login');
-Route::post('login',[SessionsController::class, 'store'])->name('login');
-Route::delete('logout',[SessionsController::class, 'destroy'])->name('logout');
+Route::get('login', [SessionsController::class, 'create'])->name('login');
+Route::post('login', [SessionsController::class, 'store'])->name('login');
+Route::delete('logout', [SessionsController::class, 'destroy'])->name('logout');
+Route::get('users/{user}/edit', [UsersController::class, 'edit'])->name('users.edit');
