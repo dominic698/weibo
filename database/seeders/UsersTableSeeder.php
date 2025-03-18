0f6d7d1ea1d2b16c323ff3e9bf464c3bdfd5ee99
dominic <zzp208@gmail.com>
2025-03-18 10:20:49 +0800

    email-pwd

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,8 +17,9 @@ class UsersTableSeeder extends Seeder
         User::factory()->count(50)->create();
         $user = User::find(1);
         $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->email = 'zzp208@gmail.com';
         $user->is_admin = true;
+        $user->password = bcrypt('123456');
         $user->save();
     }
 }
