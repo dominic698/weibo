9ee744fdfa4af9e412b4d67bb87ca3e237bc503e
dominic <zzp208@gmail.com>
2025-03-19 11:55:55 +0800

    emai

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -14,7 +14,7 @@ class PasswordController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('throttle:3,10', [
+        $this->middleware('throttle:100,10', [
             'only' => ['sendResetLinkEmail'],
         ]);
     }
